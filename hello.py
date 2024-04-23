a8afb7903fe9effdb3149eea610eadcbf1cbe3a5
Harshdeepsinh Zala <85785534+hdz-088@users.noreply.github.com>
2024-04-23 12:06:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello World")
+"""
+Title: Program to print "Hello World!"
+Author: HDz(https://github.com/hdz-088/)
+Date of Creation: January 31st, 2024
+Last Update: April 23rd, 2024
+"""
+
+print("Hello World!")
