a5dbd86a73ddc9af2005894e594a0d4ffab3d931
Harshdeepsinh Zala <85785534+hdz-088@users.noreply.github.com>
2024-04-23 12:07:19 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,10 @@
+"""
+Title: Program to Check Whether Given List is Palindrome or Not
+Author: HDz(https://github.com/hdz-088/)
+Date of Creation: January 31st, 2024
+Last Update: April 23rd, 2024
+"""
+
 list = [1, 2, 3, 2, 1]
 
 copy_list = list.copy()
