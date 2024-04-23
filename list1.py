56d51eb0b89777ab55d25020af80a8925c78bc72
Harshdeepsinh Zala <85785534+hdz-088@users.noreply.github.com>
2024-04-23 12:06:45 +0530

    Update list1.py

--- list1.py
+++ list1.py
@@ -1,4 +1,9 @@
-#Program to ask the user to enter names of their 3 favorite movies and store them in a list
+"""
+Title: Program to ask the user to enter names of their 3 favorite movies and store them in a list
+Author: HDz(https://github.com/hdz-088/)
+Date of Creation: January 31st, 2024
+Last Update: April 23rd, 2024
+"""
 
 a = input("Enter 1st name:")
 b = input("Enter 2nd name:")
