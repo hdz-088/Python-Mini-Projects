dd529cc0605a42f6b95814bebe9c81bd10777845
Harshdeepsinh Zala <85785534+hdz-088@users.noreply.github.com>
2024-04-23 12:05:32 +0530

    Update Programs

--- calender.py
+++ calender.py
@@ -1,6 +1,13 @@
+"""
+Title: Program to print Calendar
+Author: HDz(https://github.com/hdz-088/)
+Date of Creation: January 31st, 2024
+Last Update: April 23rd, 2024
+"""
+
 import calendar
 
-yy = 2024
-mm = 1
+yy = int(input("Enter Year: "))
+mm = int(input("Enter Month: "))
 
 print(calendar.month(yy, mm))
